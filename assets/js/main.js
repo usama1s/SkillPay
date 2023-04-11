d5448a5dfd595637a5cdbd53023bcf3b548a2fe0
usama1s <usama_1s@hotmail.com>
2023-04-11 15:46:40 +0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,5 +92,48 @@ function togglesidebar() {
     closebtn.style.display="block"; 
   }
 sidebar.classList.toggle("sidebarhide");
-contentside.classList.toggle("content-side")
+contentside.classList.toggle("content-side");
+contentside.classList.toggle("w-100");
+
 }
+
+
+
+
+
+
+const fileInput = document.getElementById("file-input");
+      const fileUpload = document.querySelector(".file-upload");
+      const feedback = document.getElementById("feedback");
+
+      fileUpload.addEventListener("dragover", (event) => {
+        event.preventDefault();
+        fileUpload.classList.add("dragover");
+      });
+
+      fileUpload.addEventListener("dragleave", () => {
+        fileUpload.classList.remove("dragover");
+      });
+
+      fileUpload.addEventListener("drop", (event) => {
+        event.preventDefault();
+        fileUpload.classList.remove("dragover");
+        const file = event.dataTransfer.files[0];
+        handleFile(file);
+      });
+
+      fileInput.addEventListener("change", (event) => {
+        const file = event.target.files[0];
+        handleFile(file);
+      });
+
+      function handleFile(file) {
+        // Show a message while the file is being uploaded
+        feedback.textContent = "Uploading file...";
+
+        // Simulate an upload process with a setTimeout
+        setTimeout(() => {
+          // Do something with the file
+          feedback.textContent = `File "${file.name}" uploaded successfully`;
+        }, 2000);
+      }
